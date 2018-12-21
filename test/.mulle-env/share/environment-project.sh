3934203023ac812aa5ae1e235a9bbe8a25f2cbfc
Nat! <nat@codeon.de>
2018-12-21 15:26:40 +0100

    add test files

---
+++ test/.mulle-env/share/environment-project.sh
@@ -0,0 +1,48 @@
+#
+#
+#
+export PROJECT_NAME="MulleObjCExpatFoundation"
+
+
+#
+#
+#
+export PROJECT_IDENTIFIER="MulleObjCExpatFoundation"
+
+
+#
+#
+#
+export PROJECT_DOWNCASE_IDENTIFIER="mulle_objc_expat_foundation"
+
+
+#
+#
+#
+export PROJECT_UPCASE_IDENTIFIER="MULLE_OBJC_EXPAT_FOUNDATION"
+
+
+#
+#
+#
+export PROJECT_TYPE="none"
+
+
+#
+#
+#
+export PROJECT_LANGUAGE="c"
+
+
+#
+#
+#
+export PROJECT_DIALECT="objc"
+
+
+#
+#
+#
+export PROJECT_EXTENSIONS="m:aam"
+
+
