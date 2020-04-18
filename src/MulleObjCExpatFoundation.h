0145a8ab27c0eba6f7776fb5744e95bbb3502dea
Nat! <nat@mulle-kybernetik.com>
2020-04-19 01:43:59 +0200

    * remove duplicate objc-loader.inc

--- src/MulleObjCExpatFoundation.h
+++ src/MulleObjCExpatFoundation.h
@@ -7,7 +7,7 @@
 //
 #import "import.h"
 
-#define MULLE_OBJC_EXPAT_FOUNDATION_VERSION  ((0 << 20) | (17 << 8) | 1)
+#define MULLE_OBJC_EXPAT_FOUNDATION_VERSION  ((0 << 20) | (17 << 8) | 2)
 
 #import "MulleObjCLoader+MulleObjCExpatFoundation.h"
 
