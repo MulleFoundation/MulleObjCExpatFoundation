623cc6e768525654ed25c883215cc91828a6da69
Nat! <nat@mulle-kybernetik.com>
2026-01-13 22:45:01 +0100

    maintenance: upgrade mulle-sde tooling versions

--- src/MulleObjCExpatFoundation.h
+++ src/MulleObjCExpatFoundation.h
@@ -7,7 +7,7 @@
 //
 #import "import.h"
 
-#define MULLE_OBJC_EXPAT_FOUNDATION_VERSION  ((0UL << 20) | (20 << 8) | 7)
+#define MULLE_OBJC_EXPAT_FOUNDATION_VERSION  ((0UL << 20) | (20 << 8) | 8)
 
 #import "_MulleObjCExpatFoundation-export.h"
 #import "_MulleObjCExpatFoundation-provide.h"
